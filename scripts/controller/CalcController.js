0ccf9e5be4e6d3348142bdc581e14184cdb34741
Diego Santos <diegocontasantos@gmail.com>
2022-08-30 01:06:17 -0300

    Alteração
    formatação do display da calculadora

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,3 +1,87 @@
-calc = new CalcController;
+class CalcController{
 
-let join
+    constructor()
+        {
+            this._locale = 'pt-BR';
+            this._displayCalcEl = document.querySelector("#display");
+            this._dateEl = document.querySelector("#data");
+            this._timeEl =  document.querySelector("#hora");
+
+            this._currentDate;
+            this.initialize();
+        }
+
+        initialize()
+        {
+            this.setDisplayDateTime();
+
+          setInterval (()=>{
+
+            this.setDisplayDateTime();
+
+          },1000);
+        }
+
+        /*Selecionar todos os botões através do querry selector para adicionar os eventos de click*/
+        initButtonsEvents()
+            {
+               let buttons = document.querySelectorAll("#buttons > g, #parts > g")
+
+
+            }
+        setDisplayDateTime()
+        {
+            this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
+                day: "2-digit",
+                month: "long",
+                year: "numeric"
+            });
+            this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+        }
+    
+
+
+
+    get displayTime()
+        {
+            return this._timeEl.innerHTML;
+        }
+
+    set displayTime(value)
+        {
+            return this._timeEl.innerHTML = value;
+            
+        }
+
+    get displayDate()
+        {
+            return this._dateEl.innerHTML;
+        }
+
+    set displayDate(value)
+        {
+            return this._dateEl.innerHTML = value;            
+        }
+        
+
+    get displayCalc() 
+        {
+            return this._displayCalcEl.innerHTML;
+        }
+
+    set displayCalc(value)
+        {
+            this._displayCalcEl.innerHTML = value;
+        }
+
+    get currentDate()
+        {
+            return new Date();
+        }
+
+    set currentDate(value)
+        {
+            this._currentDate = value;   
+        }
+
+}
